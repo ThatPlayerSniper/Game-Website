b1207f12acf33eacdbed456ef384bf6d73200af8
Tomás <114113569+ThatPlayerSniper@users.noreply.github.com>
2024-04-30 03:31:12 +0100

    update 2
    fixing
    creating

--- responsiveNavBar.js
+++ responsiveNavBar.js
@@ -1,9 +1,8 @@
-//Responsive Nav bar
 function myNavBar() {
     var x = document.getElementById("navBarResposive");
     if (x.className === "navBarNavi") {
         x.className += " responsive";
     } else {
-        x.className = "navBarNavi";
+        x.className = "navBarNavi"; // Change to "navBarNavi" here
     }
 }
